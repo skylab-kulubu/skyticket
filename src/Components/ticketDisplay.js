01a434c25a28dd279296557b8c127a5cd2bae0c3
Emre Uslu <75951256+rakun256@users.noreply.github.com>
2024-11-10 18:10:31 +0300

    Stamp added.

--- src/Components/ticketDisplay.js
+++ src/Components/ticketDisplay.js
@@ -1,44 +1,38 @@
+
 import React, { useEffect, useRef, useState } from "react";
 import ColorThief from "colorthief";
 import { submitTicket } from "../Services/ticketService";
 
 const TicketDisplay = ({ color, character, ticketId }) => {
   const imagePath = require(`../Tickets/${color}/${character}.png`);
-  const stampPath = require("../Tickets/ticket-stamp.png"); // Damga görüntüsünün yolu
+  const stampPath = require(`../Tickets/ticket-stamp.png`);
   const imgRef = useRef(null);
-  const [bgColor, setBgColor] = useState("#f0f4f8"); // Varsayılan arka plan rengi
-  const [stampPosition, setStampPosition] = useState(null); // Damga pozisyonu için durum
+  const [bgColor, setBgColor] = useState("#f0f4f8");
+  const [stampPosition, setStampPosition] = useState(null);
 
   const handleTouchStart = (event) => {
     if (event.touches.length === 3) {
-      console.log("Üç parmakla basıldı, damga işlemi başlatılıyor...");
-      const touch = event.touches[0]; // İlk dokunulan noktayı al
-      const x = touch.clientX;
-      const y = touch.clientY;
-
-      setStampPosition({ x, y }); // Damganın konumunu kaydet
-      handleStamp(); // Damga fonksiyonunu çağır
+      const touch = event.touches[0];
+      setStampPosition({ x: touch.clientX, y: touch.clientY });
+      handleStamp();
     }
   };
 
   const handleStamp = async () => {
     try {
-      console.log(`Ticket ID: ${ticketId} için PUT isteği gönderiliyor...`);
       const result = await submitTicket(ticketId);
-      console.log("Bilet başarıyla damgalandı:", result);
       alert("Bilet başarıyla kullanıldı!");
     } catch (error) {
-      console.error("Bilet zaten kullanıldı veya başka bir hata oluştu:", error);
       alert(error.message || "Bilet zaten kullanıldı!");
     }
   };
 
   useEffect(() => {
     const colorThief = new ColorThief();
-
     const handleImageLoad = () => {
-      if (imgRef.current) {
-        const color = colorThief.getColor(imgRef.current);
+      const currentImgRef = imgRef.current;
+      if (currentImgRef) {
+        const color = colorThief.getColor(currentImgRef);
         setBgColor(`rgb(${color[0]}, ${color[1]}, ${color[2]})`);
       }
     };
@@ -66,14 +60,13 @@ const TicketDisplay = ({ color, character, ticketId }) => {
         ref={imgRef}
         crossOrigin="anonymous"
       />
-      {/* Damga görüntüsü */}
       {stampPosition && (
         <img
           src={stampPath}
           alt="Stamp"
           className="stamp"
           style={{
-            top: stampPosition.y - 25, // Damganın merkezini konumlandır
+            top: stampPosition.y - 25,
             left: stampPosition.x - 25,
           }}
         />
