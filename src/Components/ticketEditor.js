fec6e87230279965e83cc46dc8ec533cbdaef833
Emre Uslu <75951256+rakun256@users.noreply.github.com>
2024-11-16 17:21:43 +0300

    name bug fixed

--- src/Components/ticketEditor.js
+++ src/Components/ticketEditor.js
@@ -25,10 +25,10 @@ const TicketEditor = ({ color, character, owner, onImageReady, imgRef }) => {
           // Yazıyı ekle
           ctx.font = "80px Bebas Neue";
           ctx.fillStyle = "white";
-          ctx.textAlign = "center";
+          ctx.textAlign = "left";
           ctx.fillText(
             `${owner.firstName} ${owner.lastName}`,
-            canvas.width * 0.2,
+            canvas.width * 0.13,
             canvas.height * 0.6
           );
 
